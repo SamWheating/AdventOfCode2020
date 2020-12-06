26142cd5d395e1b794b13f9ee6416fc077cef7d1
Sam Wheating <sam.wheating@shopify.com>
2020-12-06 15:39:33 -0500

    Day 6

---
+++ 6/solution.py
@@ -0,0 +1,23 @@
+import string
+
+with open('6/input.txt') as inputfile:
+    groups = inputfile.read().split("\n\n")
+
+def part1():
+    count = 0
+    for group in groups:
+        count += len(set(group.replace("\n", "")))
+    return count
+
+def part2():
+    count = 0
+    for group in groups:
+        intersection = set(list(string.ascii_lowercase))
+        for person in group.split("\n"):
+            intersection = intersection & set(list(person))
+        count += len(intersection)
+    return count
+
+if __name__ == "__main__":
+    print(f"Part 1 solution: {part1()}")
+    print(f"Part 2 solution: {part2()}")
