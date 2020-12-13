21e4b39fc35133c3ba13d3091d0c10ece99471a0
Sam Wheating <sam.wheating@shopify.com>
2020-12-13 01:49:44 -0500

    cleanup

--- 13/solution.py
+++ 13/solution.py
@@ -10,12 +10,11 @@ def part1():
         delays.append(((int(departure/bus)+1) * bus) - departure)
     return bus_times[delays.index(min(delays))] * min(delays)
 
-# finds the generating function which encapsulates two other generating functions
+# experimentally finds the generating function which encapsulates two other generating functions
 # i.e generating function which provides all numbers x such that x = Ax + B = Cy + D
 def find_new_generator(period1, offset1, period2, offset2):
     i = 0
     initial = None
-    period = None
     while True:
         if (i*period1 + offset1 - offset2) % period2 == 0:
             if initial is None:
