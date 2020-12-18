fc5229a904421883c62011cf9968f25f83b910e4
Sam Wheating <sam.wheating@shopify.com>
2020-12-18 00:48:27 -0500

    typos + cleanup

--- 18/solution.py
+++ 18/solution.py
@@ -1,5 +1,5 @@
 
-# evaluates an equation with no brackets form left to right
+# evaluates an equation with no parantheses from left to right
 # ex. eval_eq([1, '+', 2]) -> 3 
 def eval_eq_1(equation):
     if len(equation) == 1:
@@ -13,11 +13,10 @@ def eval_eq_1(equation):
         new_eq.extend(equation[3:])
         return eval_eq_1(new_eq)
 
-# evaluates an equation with no brackets form left to right
+# evaluates an equation with no parantheses from left to right
 # but with all additions performed before any multiplications
 # ex. eval_eq([5, '*', 1, '+', 2]) -> 15
 def eval_eq_2(equation):
-    print(equation)
     if len(equation) == 1:
         return int(equation[0])
     elif "+" in equation:
@@ -32,8 +31,8 @@ def eval_eq_2(equation):
         return eval_eq_2(new_eq)
 
 # Solves a long expression recursively by solving innermost parantheses on each iteration
+# operates on a list of numbers and operators ex [5, '*', '(', 1, '+', 2, ')']
 def solve(equation, eval_method):
-    print(f"solving {equation}")
     if ")" not in equation:
         return eval_method(equation)
     end = equation.index(")")
@@ -41,7 +40,6 @@ def solve(equation, eval_method):
     while equation[start] != "(":
         start -= 1
     new_equation = equation[:start] + [eval_method(equation[start+1:end])] + equation[end+1:]
-    print(new_equation)
     return solve(new_equation, eval_method)
 
 def part1(inputlines):
