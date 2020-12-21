9b98764d9274e6e0f345af6a825ec9f58b2c0651
Sam Wheating <sam.wheating@shopify.com>
2020-12-21 00:50:53 -0500

    cleanup

--- 21/solution.py
+++ 21/solution.py
@@ -1,27 +1,26 @@
-import copy
-import math
+def find_allergens(inputlines):
 
-def part1(inputlines):
-    allergen_map = {}  # make a mapping of alergens to all possible ingredients
-    all_ingredients = set()
+    allergen_map = {}  # make a mapping of allergens to all possible ingredients
     for line in inputlines:
         allergens = line.split("(contains ")[1][:-1].split(", ")
         ingredients = line.split(" (contains")[0].split(" ")
-        all_ingredients.update(ingredients)
         for allergen in allergens:
             if allergen in allergen_map:
                 allergen_map[allergen].append(set(ingredients))
             else:
                 allergen_map[allergen] = [set(ingredients)]
-        print(allergens)
+
     unresolved_ingredient_map = {} # maps allergen to possible ingredients
-    resolved_ingredient_map = {}
+    resolved_ingredient_map = {} # maps allergen to ingredients
+
+    # find the intersection of ingredients for every recipe which contains this allergen
     for allergen in allergen_map:
         possible_ingredients = allergen_map[allergen][0]
         for i in range(1, len(allergen_map[allergen])):
             possible_ingredients = possible_ingredients & allergen_map[allergen][i]
         unresolved_ingredient_map[allergen] = possible_ingredients
 
+    # iteratively solve mapping between ingredients and allergens
     while True:
         for allergen, ingredients in unresolved_ingredient_map.items():
             if len(ingredients) == 1:
@@ -31,58 +30,25 @@ def part1(inputlines):
                     unresolved_ingredient_map[a].discard(ingredient)
                 break
         if len(resolved_ingredient_map) == len(unresolved_ingredient_map):
-            break
+            return resolved_ingredient_map
 
-    # now we know exactly which allergens in which ingredient
-    print(resolved_ingredient_map)
-    # print(all_ingredients)
-    unsafe_ingredients = list(resolved_ingredient_map.values())
-    safe_ingredients = set([ingredient for ingredient in all_ingredients if ingredient not in unsafe_ingredients])
+
+def part1(inputlines):
+
+    unsafe_ingredients = list(find_allergens(inputlines).values())
 
     count = 0
     for line in inputlines:
         ingredients = line.split(" (contains")[0].split(" ")
-        for ingredient in ingredients:
-            if ingredient in safe_ingredients:
-                count += 1
+        count += len([a for a in ingredients if a not in unsafe_ingredients])
 
     return count
 
 
 def part2(inputlines):
-    allergen_map = {}  # make a mapping of alergens to all possible ingredients
-    all_ingredients = set()
-    for line in inputlines:
-        allergens = line.split("(contains ")[1][:-1].split(", ")
-        ingredients = line.split(" (contains")[0].split(" ")
-        all_ingredients.update(ingredients)
-        for allergen in allergens:
-            if allergen in allergen_map:
-                allergen_map[allergen].append(set(ingredients))
-            else:
-                allergen_map[allergen] = [set(ingredients)]
-        print(allergens)
-    unresolved_ingredient_map = {} # maps allergen to possible ingredients
-    resolved_ingredient_map = {}
-    for allergen in allergen_map:
-        possible_ingredients = allergen_map[allergen][0]
-        for i in range(1, len(allergen_map[allergen])):
-            possible_ingredients = possible_ingredients & allergen_map[allergen][i]
-        unresolved_ingredient_map[allergen] = possible_ingredients
-
-    while True:
-        for allergen, ingredients in unresolved_ingredient_map.items():
-            if len(ingredients) == 1:
-                ingredient = list(ingredients)[0]
-                resolved_ingredient_map[allergen] = ingredient
-                for a in unresolved_ingredient_map:
-                    unresolved_ingredient_map[a].discard(ingredient)
-                break
-        if len(resolved_ingredient_map) == len(unresolved_ingredient_map):
-            break
     
+    resolved_ingredient_map = find_allergens(inputlines)
     allergens_sorted = sorted(list(resolved_ingredient_map.keys()))
-    print(allergens_sorted)
     solution = ",".join(resolved_ingredient_map[allergen] for allergen in allergens_sorted)
     return solution
 
